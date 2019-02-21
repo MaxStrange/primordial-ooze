cbe0069f9d20b0c61387ac6833ae66c8e63c5b9e
Max Strange <maxfieldstrange@gmail.com>
2019-02-21 14:46:50 -0800

    WIP

--- primordialooze/primordialooze.py
+++ primordialooze/primordialooze.py
@@ -5,6 +5,8 @@ that are out there.
 
 See the README or the docstrings in this file for the documentation.
 """
+import multiprocessing
+import numpy as np
 
 class Simulation:
     """
@@ -13,11 +15,117 @@ class Simulation:
     ```python
     import primordialooze as po
 
-    sim = po.Simulation()
+    sim = po.Simulation(nagents, shape, fitnessfunction)
     bestagent, fitness = sim.run()
     print(sim.results)
     ```
+
+    ## Args
+
+    The following list contains the arguments that are needed. These do not have default values
+    since the values for these will change dramatically depending on the problem.
+
+    - **population**: The number of agents in the first generation. We will generate this many agents
+                      in the initial generation, each of which is a Numpy Array of shape=`shape`.
+                      They will be mutated according to `mutationfunc`, and evaluated each generation
+                      by `fitnessfunc`.
+    - **shape**: The shape of each agent in the population. Must be a list-like.
+    - **fitnessfunc**: The function to use to evaluate the fitness of each agent in the generation.
+                       Must have signature: `def fitnessfunc(agent) -> scalar float`. This function
+                       will be evaluated on every single agent in the gene pool at each generation.
+                       If this function is slow, it probably makes sense to use multiprocessing.
+                       See `nwokers`.
+
+    ## Keyword Args
+
+    These arguments contain (mostly) sensible defaults, but you should definitely make sure these
+    defaults work for you. You will almost certainly want to change some of these to fit your problem.
+
+    - **seedfunc**: The function to use to create the first generation of agents. The function must have
+                    the signature `def seedfunc() -> agent of shape 'shape'`. We call this function
+                    `population` times. When `None`, defaults to uniform random
+                    over the range [-1.0, 1.0) in each dimension.
+    - **selectionfunc**: The function to use to select the agents that are allowed to breed to create the
+                         next generation. Signature must be `def selectionfunc(population, evaluations) -> selected_agents`,
+                         where `population` is an n-dimensional array of shape (nagents, *agent_shape),
+                         `evaluations` is an array of shape (nagents,); `evaluations[i]` contains
+                         the fitness value for `population[i]`; `selected_agents` is an n-dimensional array
+                         of shape (nagents_selected, *agent_shape), which must contain the selected agents.
+                         `population` and `evaluations` are pre-sorted so that `population[0]`, corresponds
+                         to `evalutaion[0]` and has the highest evaluation value.
+                         When `None`, defaults to selecting the top ten percent.
+    - **crossoverfunc**:
+    - **mutationfunc**:
+    - **elitismfunc**: A function of signature `elitismfunc(generation_index) -> float in range [0.0, 1.0]`.
+                       This function takes the index of the generation (0 for the first generation, 1 for the second, etc.)
+                       and returns the fraction of top-performers to hold over as-is to the next generation.
+                       When `None`, defaults to a function that simply returns 0.1 (or 10%) of the gene pool regardless of the
+                       generation.
+    - **nworkers**: The number of processes to use to parallelize the various functions. This will default to 0, which will
+                    mean no parallelism at all. `None` will use the number of cores. Otherwise, must be an integer.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, population, shape, fitnessfunc, *, seedfunc=None, selectionfunc=None,
+                    crossoverfunc=None, mutationfunc=None, elitismfunc=None, nworkers=0):
+        # Validate population
+        if population <= 0:
+            raise ValueError("Population must be > 0 but is {}".format(population))
+        population = int(population)
+
+        # Validate shape
+        for i, dim in enumerate(shape):
+            if dim <= 0:
+                raise ValueError("Shape must contain no negative values, but contains {} at index {}".format(dim, i))
+        try:
+            _testagent = np.ndarray(shape=shape)
+        except Exception:
+            raise ValueError("There is something wrong with your shape parameter. It must be a list-like of integers greater than zero but is: {}.".format(shape))
+
+        # Do not validate functions; may take too long
+
+        # Validate nworkers
+        if nworkers is None:
+            nworkers = multiprocessing.cpu_count()
+            if nworkers <= 0:
+                raise ValueError("Something is wrong with multiprocessing.cpu_count(). Try passing in a number for nworkers instead of None.")
+        elif nworkers < 0:
+            raise ValueError("Nworkers must be zero (for no multiprocessing), None, or a positive integer, but is: {}".format(nworkers))
+        nworkers = int(nworkers)
+
+        self._initial_population_size = population
+        self._shape = shape
+        self._fitnessfunc = fitnessfunc
+        self._seedfunc = self._default_seedfunc if seedfunc is None else seedfunc
+        self._selectionfunc = self._default_selectionfunc if selectionfunc is None else selectionfunc
+        # TODO: more
+
+    def _default_seedfunc(self):
+        """
+        Default seed function to create the first generation of agents. Each time this is called, it creates
+        a new agent from uniform random of shape `self._shape` over the values[-1.0, 1.0).
+        """
+        # TODO: Test default function only produces individuals in range [-1.0, 1.0)
+        return np.random.uniform(low=-1.0, high=1.0, size=self._shape)
+
+    def _default_selectionfunc(self, population, evaluations):
+        """
+        Default selection function for selecting agents allowed to breed to create the next generation.
+        Simply takes the top 10% of the given population and return them.
+
+        Population and evaluations are pre-sorted so that index 0 is the fittest.
+
+        This is guaranteed to take at least one agent.
+        """
+        # TODO: Test default function takes the top ten percent (and not just some random ten percent)
+        tenpercent = int(population.shape[0] * 0.1)
+        if tenpercent < 1:
+            tenpercent = 1
+
+        # Flatten the population into 2D - shape (nagents, flattened_agent)
+        reshaped = np.reshape(population, (-1, np.product(population.shape[1:])))
+
+        # Take the top ten percent
+        topperformers = reshaped[0:tenpercent, :]
+
+        # Reshape and return
+        return np.reshape(topperformers, (tenpercent, *population.shape[1:]))
--- tests.py
+++ tests.py
@@ -14,5 +14,24 @@ class TestOoze(unittest.TestCase):
         """
         _ = po.Simulation()
 
+    def test_solve_conic(self):
+        """
+        Test that we can solve a simple 2D upside-down parabola problem.
+        """
+        def fitness(agent):
+            """
+            This is an upside-down parabola in 2D with max value at (z = (x, y) = 5 = (0, 0)).
+            """
+            x = agent[0]
+            y = agent[1]
+            return (-1.2 * x**2) - (0.75 * y**2) + 5
+
+        nagents = 100
+        sim = po.Simulation(nagents, fitnessfunc=fitness)
+        best, value = sim.run()
+        self.assertAlmostEqual(best[0], 0.0, places=4)
+        self.assertAlmostEqual(best[1], 0.0, places=4)
+        self.assertAlmostEqual(value, 5.0, places=4)
+
 if __name__ == '__main__':
     unittest.main()
